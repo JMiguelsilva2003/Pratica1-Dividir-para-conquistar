5380553aa5e973084d3c5cb3eb047ec52f6492f5
JMiguelsilva2003 <jmigueldasilva953@gmail.com>
2024-04-14 13:25:02 -0300

    Prática 1 - Dividir para conquistar

---
+++ Pratica1/src/BuscaBinariaRecursiva.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class BuscaBinariaRecursiva {
+    public static void main(String[] args) throws Exception {
+        int array[] = {3, 5, 6, 7, 10, 12, 1, 2};
+        Arrays.sort(array);
+        int elemento = 7;
+        int resultado = buscaBinariaRecursiva(array, elemento);
+        if (resultado < 0) {
+            System.err.println("Elemento não encontrado");
+        } else {
+            System.out.println("| Elemento encontrado no índice: " + resultado + " | na posição: " + (resultado + 1) + " |");
+        }
+    }
+
+    public static int buscaBinariaRecursiva(int array[], int elemento) {
+        return buscaRecursiva(array, elemento, 0, array.length - 1);
+    }
+
+    public static int buscaRecursiva(int array[], int elemento, int menor, int maior) {
+        if (menor > maior) {
+            return -1;
+        }
+        int media = (menor + maior) / 2;
+        if (array[media] == elemento) {
+            return media;
+        }
+        if (array[media] < elemento) {
+            return buscaRecursiva(array, elemento, media + 1, maior);
+        } else {
+            return buscaRecursiva(array, elemento, menor, media - 1);
+        }
+    }
+}
